6d3415afcb481dd94ed210aed52a8a1e4801d161
Tebin Ulrich <tebin.ulrich@frantos.com>
2023-01-25 18:10:20 +0100

    Some more dynamic fetching of elements

--- Immoscout24Helper.php
+++ Immoscout24Helper.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * https://www.google.com/maps/d/edit?hl=de&mid=1fdhmdVZvh_hwodpJM6_gfKu1j2adwuk&ll=47.54793160709045%2C8.979375164330271&z=11
- *
- * Favorites aus dem Ajax Return holen und in favorites.json speichern.
+ * Get your favorites from the ajax response on the favorites page of immoscout24.ch and copy the content to favorites.json
+ * Then start this script (call index.php) and wait. Afterwards, a CSV file is generated (places.csv).
+ * Go to https://www.google.com/maps/d/ -> create new map -> import generated CSV
  */
 class Immoscout24Helper
 {
@@ -11,6 +11,11 @@ class Immoscout24Helper
      * @var string
      */
     protected string $url = 'https://www.immoscout24.ch/en/d/flat-rent-bern/';
+
+    /**
+     * @var string
+     */
+    protected string $inputFilename = 'favorites.json';
 
     /**
      * @var string
@@ -27,7 +32,7 @@ class Immoscout24Helper
      */
     public function __construct()
     {
-        $file = file_get_contents('favorites.json');
+        $file = file_get_contents($this->getInputFilename());
         $json = json_decode($file, false, 512, JSON_THROW_ON_ERROR);
 
         $this->setFavorites($json->favourites);
@@ -44,28 +49,34 @@ class Immoscout24Helper
         foreach ($this->getFavorites() as $favorite) {
             $url            = $this->getUrl() . $favorite->propertyId;
             $crawledContent = $this->getContentByUrl($url);
-            $info           = $this->getInformation($crawledContent);
-
-            $info[3] = $url;
-            if (!str_contains($info[1], 'immo') && !str_contains($info[1], 'Immo') && strlen($info[1]) < 15 && !str_contains($info[2], 'immoscout24.ch')) {
-                fputcsv($csv, array_slice($info, 0, 4));
-            }
+            $info           = $this->getInformation($crawledContent, $url);
+            fputcsv($csv, $info);
         }
     }
 
 
     /**
      * @param string $crawledContent
+     * @param string $url
      *
      * @return string[]
      */
-    public function getInformation(string $crawledContent): array
+    public function getInformation(string $crawledContent, string $url): array
     {
         $doc = new DOMDocument();
         $doc->loadHTML($crawledContent);
-        $text = $doc->childNodes->item(1)->textContent;
+        $xpath = new DOMXPath($doc);
 
-        return explode(' - ', trim(str_replace('\n', '', explode('|', $text)[0])));
+        $title    = $xpath->evaluate('//article/h2')->item(0)->textContent;
+        $price    = $xpath->evaluate('//article/div/h2')->item(0)->textContent;
+        $location = $xpath->evaluate('//article/p')->item(0)->textContent;
+
+        return [
+            $location,
+            $price,
+            $title,
+            $url,
+        ];
     }
 
 
@@ -137,4 +148,14 @@ class Immoscout24Helper
     {
         return $this->outputFilename;
     }
+
+    /**
+     * @return string
+     */
+    public function getInputFilename(): string
+    {
+        return $this->inputFilename;
+    }
+
+
 }
